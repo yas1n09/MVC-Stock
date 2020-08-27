2a554c7cc63d4b040aed790bebfc5e083a798bca
yas1n09 <yasinyagci0990@gmail.com>
2020-08-27 16:45:01 +0300

    MVC-Stock
    Incudes DataBase Backup

---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using $safeprojectname$.Models.Entity;
+
+namespace $safeprojectname$.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDBStokEntities db = new MvcDBStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
